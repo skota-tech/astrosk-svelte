c76026fe9756ced2427b0dee8bd9cec2aee5e7b4
skota-tech <96097938+skota-tech@users.noreply.github.com>
2025-01-31 05:52:32 -0600

    Replacing pnpm with npm

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+
+	assetsInclude: ['ephe']
 });
